d879203e27bdae4012eb81f647137088dac24ed0
bengroves <bengroves15@gmail.com>
2020-11-03 13:53:23 -0500

    commit tree 37178c5262764cf3e7b0e2a5755bb4a0b7725d95

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+
+class Dog
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+    def save
+        @@all << self
+    end
+    def self.all
+        @@all
+    end 
+
+    def name
+        @name
+    end
+
+    def self.clear_all
+        @@all = []
+    end 
+
+    def self.print_all
+        i = 0
+        runner = (1..@@all.length).to_a
+        for i in runner
+            keep = @@all[i-1]
+            puts keep.name
+        end 
+
+    end
+end 
